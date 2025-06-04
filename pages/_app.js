6e6746862ff5ee3d2100b0317a9946bbc55f6785
Taytaytron <tayyaryldzoglu@gmail.com>
2025-06-04 15:32:26 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,1 +1,11 @@
-export default function App({ Component, pageProps }) { return <Component {...pageProps} />; }
+import '../styles/globals.css';
+import Header from '../components/Header';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
+}
