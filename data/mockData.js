1fb395d5c1f0cd7590ecbd33f1ad95a761f722e9
Taytaytron <tayyaryldzoglu@gmail.com>
2025-06-04 15:35:00 +0300

    Update mockData.js

--- data/mockData.js
+++ data/mockData.js
@@ -1,1 +1,16 @@
-export const books = [{ title: 'Book 1' }, { title: 'Book 2' }];
+export const books = [
+  {
+    id: 1,
+    title: "The Alchemist",
+    author: "Paulo Coelho",
+    description: "A journey of self-discovery.",
+    image: "https://picsum.photos/seed/alchemist/200/300"
+  },
+  {
+    id: 2,
+    title: "Atomic Habits",
+    author: "James Clear",
+    description: "How tiny changes make big results.",
+    image: "https://picsum.photos/seed/habits/200/300"
+  }
+];
